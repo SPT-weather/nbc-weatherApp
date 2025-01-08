63dfe4d2635ea0450068cf12ab75763b3172096f
soy <soysauce1223@naver.com>
2025-01-08 20:21:44 +0900

    ✨ [Feat] 시간별날씨 Cell UI 설정

---
+++ GoWalk/GoWalk/Detail/View/HourlyCollectionViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  HourlyCollectionViewCell.swift
+//  GoWalk
+//
+//  Created by seohuibaek on 1/8/25.
+//
+
+import UIKit
+import SnapKit
+
+class HourlyCollectionViewCell: UICollectionViewCell {
+    static let identifier = "HourlyCollectionViewCell"
+
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .black
+        label.text = "오후 1시"
+        return label
+    }()
+
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "DummyWeather")
+        return imageView
+    }()
+
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14, weight: .medium)
+        label.textColor = .darkGray
+        label.text = "1도"
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        contentView.addSubview(timeLabel)
+        contentView.addSubview(temperatureLabel)
+        contentView.addSubview(iconImageView)
+
+        timeLabel.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview().inset(8)
+        }
+
+        iconImageView.snp.makeConstraints { make in
+            make.top.equalTo(timeLabel.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(24)
+        }
+
+        temperatureLabel.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview().inset(8)
+        }
+    }
+
+//    func configure(with model: HourlyForecast) {
+//        timeLabel.text = model.time
+//        temperatureLabel.text = model.temperature
+//        iconImageView.image = UIImage(systemName: model.icon)
+//    }
+}
