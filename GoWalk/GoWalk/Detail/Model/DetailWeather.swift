dd7fec8e2d1b54450f57027b596c18c8ce0578f5
soy <soysauce1223@naver.com>
2025-01-13 02:18:11 +0900

    🎨 [Chore] 더미 데이터 코드 삭제 및 icon 변수 삭제

--- GoWalk/GoWalk/Detail/Model/DetailWeather.swift
+++ GoWalk/GoWalk/Detail/Model/DetailWeather.swift
@@ -8,47 +8,16 @@
 import UIKit
 
 struct DetailWeather {
-    struct Hourly {
+    struct Hourly { // 시간별 날씨
         let time: String
         let temperature: String
         let iconUrl: URL
-        
-        var icon: UIImage {
-            return UIImage(resource: .dummyWeather).withTintColor(.label)
-        }
     }
     
-    struct Weekly {
+    struct Weekly { // 주간 날씨
         let date: Date
         let iconUrl: URL
         let minTemperature: String
         let maxTemperature: String
-        
-        var icon: UIImage {
-            return UIImage(resource: .dummyWeather).withTintColor(.label)
-        }
     }
-    
-    // 더미 데이터 추가
-        static var dummyHourly: [Hourly] {
-            return (1...8).map { hour in
-                Hourly(
-                    time: "오후 \(hour)시",
-                    temperature: "11°",
-                    iconUrl: URL(string: "https://example.com")! // 실제 URL로 대체 필요
-                )
-            }
-        }
-        
-        static var dummyWeekly: [Weekly] {
-            return (0...6).map { dayOffset in
-                let date = Calendar.current.date(byAdding: .day, value: dayOffset, to: Date())!
-                return Weekly(
-                    date: date,
-                    iconUrl: URL(string: "https://example.com")!, // 실제 URL로 대체 필요
-                    minTemperature: "최저: -7°",
-                    maxTemperature: "최고: 11°"
-                )
-            }
-        }
 }
