f10a6c5a23d33cb9061735a06e151f9e95c27dbb
soy <soysauce1223@naver.com>
2025-01-13 02:35:47 +0900

    ✨ [Feat] 이미지 로더 생성

---
+++ GoWalk/GoWalk/Detail/Model/WeatherImageLoader.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherImageLoader.swift
+//  GoWalk
+//
+//  Created by seohuibaek on 1/13/25.
+//
+
+import UIKit
+
+final class WeatherImageLoader {
+    static func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url),
+                  let image = UIImage(data: data) else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+}
