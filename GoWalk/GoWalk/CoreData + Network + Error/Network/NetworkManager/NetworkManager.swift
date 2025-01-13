b13d4068814a91652906ce526c7751bc148b914a
jbnong07 <jbnong07@naver.com>
2025-01-13 23:30:00 +0900

    ✨ [Feat] fetch address, region

--- GoWalk/GoWalk/CoreData + Network + Error/Network/NetworkManager/NetworkManager.swift
+++ GoWalk/GoWalk/CoreData + Network + Error/Network/NetworkManager/NetworkManager.swift
@@ -11,10 +11,14 @@ import RxSwift
 protocol AbstractNetworkManager {
     func fetchData<DTO: Mappable>(
         url: URL,
+        header: [String: String]?,
         responseType: DTO.ResponseType.Type
     ) -> Observable<Result<DTO, AppError>>
+
     func fetchWeatherData(url: URL) -> Observable<Result<TotalWeatherDTO, AppError>>
     func fetchAirPollutionData(url: URL) -> Observable<Result<AirPollutionDTO, AppError>>
+    func fetchAddressData(url: URL, header: [String: String]) -> Observable<Result<AddressDTO, AppError>>
+    func fetchRegionData(url: URL, header: [String: String]) -> Observable<Result<RegionDTO, AppError>>
 }
 
 class RXNetworkManager: AbstractNetworkManager {
@@ -22,10 +26,17 @@ class RXNetworkManager: AbstractNetworkManager {
 
     func fetchData<DTO: Mappable> (
         url: URL,
+        header: [String: String]? = nil,
         responseType: DTO.ResponseType.Type
     ) -> Observable<Result<DTO, AppError>> {
         return Observable.create { observer in
-            let request: URLRequest = URLRequest(url: url)
+            var request: URLRequest = URLRequest(url: url)
+
+            if let header = header {
+                for (key, value) in header {
+                    request.addValue(value, forHTTPHeaderField: key)
+                }
+            }
 
             URLSession.shared.dataTask(with: request) { data, response, error in
                 if let error = error {
@@ -72,6 +83,14 @@ class RXNetworkManager: AbstractNetworkManager {
 
     func fetchAirPollutionData(url: URL) -> Observable<Result<AirPollutionDTO, AppError>> {
         return fetchData(url: url, responseType: AirPollutionDTO.ResponseType.self)
+    }
+    
+    func fetchAddressData(url: URL, header: [String: String]) -> Observable<Result<AddressDTO, AppError>> {
+        return fetchData(url: url, responseType: AddressDTO.ResponseType.self)
+    }
+    
+    func fetchRegionData(url: URL, header: [String: String]) -> Observable<Result<RegionDTO, AppError>> {
+        return fetchData(url: url, responseType: RegionDTO.ResponseType.self)
     }
 
 }
