bacce3cdf98101e3eae6bdf2e52d91f22c8b0ff0
jeff-0324 <jeffap324@gmail.com>
2025-01-13 22:29:17 +0900

    🎨 [Chore]: 코드 수정

--- GoWalk/GoWalk/AddressNetworking/AddressNetworkManager.swift
+++ GoWalk/GoWalk/AddressNetworking/AddressNetworkManager.swift
@@ -48,9 +48,8 @@ class AddressNetworkManager {
                             lat: info.lat,
                             lon: info.lon
                         )
-                        completion()
                     }
-                    
+                    completion()
                 case .failure(let error):
                     print("\(error.localizedDescription)")
                 }
