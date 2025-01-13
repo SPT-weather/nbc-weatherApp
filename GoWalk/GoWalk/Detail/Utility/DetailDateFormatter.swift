d4f0f9548a6f22b3bbdd8d8ee192ef53d7cceed8
soy <soysauce1223@naver.com>
2025-01-13 17:34:17 +0900

    ✨ [Feat] 현재, 오늘인 경우 시간 및 날짜 대신 현재, 오늘 키워드 출력

--- GoWalk/GoWalk/Detail/Utility/DetailDateFormatter.swift
+++ GoWalk/GoWalk/Detail/Utility/DetailDateFormatter.swift
@@ -10,6 +10,12 @@ import Foundation
 final class DetailDateFormatter {
     static func hourlyString(from timestamp: Int) -> String {
         let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        
+        // 현재의 시간대를 구해서 출력
+        if Int(Date().timeIntervalSince1970) / 3600 == timestamp / 3600 {
+            return "현재"
+        }
+        
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "a h시"
@@ -18,8 +24,15 @@ final class DetailDateFormatter {
 
     static func weeklyString(from timestamp: Int) -> String {
         let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MM/dd"
-        return formatter.string(from: date)
+        let now = Date()
+        
+        // 오늘 날짜인 경우
+        if Calendar.current.isDate(date, inSameDayAs: now) {
+            return "오늘"
+        } else {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "MM/dd"
+            return formatter.string(from: date)
+        }
     }
 }
