25e309504ef238ae39a0744b4fa23a136e004da4
soy <soysauce1223@naver.com>
2025-01-13 16:29:52 +0900

    🎨 [Chore] 설정 값 반영 여부를 확인하기 위한 TestModal 코드 추가

--- GoWalk/GoWalk/Detail/View/TestModalViewController.swift
+++ GoWalk/GoWalk/Detail/View/TestModalViewController.swift
@@ -2,46 +2,68 @@ import UIKit
 import SnapKit
 
 class TestModalViewController: UIViewController {
-    private lazy var button: UIButton = {
+    private lazy var modalButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Show Modal", for: .normal)
         button.addTarget(self, action: #selector(showModal), for: .touchUpInside)
         return button
     }()
-
+    
+    private lazy var settingsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Settings", for: .normal)
+        button.addTarget(self, action: #selector(showSettings), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [modalButton, settingsButton])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .center
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupUI()
     }
-
+    
     private func setupUI() {
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
             make.centerX.equalToSuperview()
         }
     }
-
+    
     @objc private func showModal() {
         let viewModel = DetailViewModel()
         let detailVC = DetailViewController(viewModel: viewModel)
-
+        
         detailVC.modalPresentationStyle = .pageSheet
         detailVC.isModalInPresentation = false
-
+        
         if let sheet = detailVC.sheetPresentationController {
             sheet.detents = [ // detents를 사용하여 커스텀
                 .custom { [weak self] _ in
                     guard let self = self else { return 500 }
-                    let buttonFrame = self.button.convert(self.button.bounds, to: self.view)
+                    let buttonFrame = self.modalButton.convert(self.modalButton.bounds, to: self.view)
                     return self.view.frame.height - buttonFrame.maxY - 10
                 }
             ]
             sheet.prefersScrollingExpandsWhenScrolledToEdge = false
             sheet.preferredCornerRadius = 20
         }
-
+        
         present(detailVC, animated: true)
+    }
+    
+    @objc private func showSettings() {
+        let settingsVC = SettingsViewController()
+        let settingsNav = UINavigationController(rootViewController: settingsVC)
+        settingsNav.modalPresentationStyle = .popover
+        present(settingsNav, animated: true)
     }
 }
