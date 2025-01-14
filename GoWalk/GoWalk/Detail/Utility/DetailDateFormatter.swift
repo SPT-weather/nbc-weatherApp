5c1b677909394b72dcaf4744cd3a00622fef1a61
soy <soysauce1223@naver.com>
2025-01-14 15:21:28 +0900

    🎨 [Chore] 불필요한 코드 삭제 및 공백 제거

--- GoWalk/GoWalk/Detail/Utility/DetailDateFormatter.swift
+++ GoWalk/GoWalk/Detail/Utility/DetailDateFormatter.swift
@@ -10,12 +10,12 @@ import Foundation
 final class DetailDateFormatter {
     static func hourlyString(from timestamp: Int) -> String {
         let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
-        
+
         // 현재의 시간대를 구해서 출력
         if Int(Date().timeIntervalSince1970) / 3600 == timestamp / 3600 {
             return "현재"
         }
-        
+
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "a h시"
@@ -25,7 +25,7 @@ final class DetailDateFormatter {
     static func weeklyString(from timestamp: Int) -> String {
         let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
         let now = Date()
-        
+
         // 오늘 날짜인 경우
         if Calendar.current.isDate(date, inSameDayAs: now) {
             return "오늘"
