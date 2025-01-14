ac5cfc2c7f87b6cf659e0df53e2bb9cf0b7e9912
jeff-0324 <jeffap324@gmail.com>
2025-01-14 15:06:45 +0900

    ✨ [Feat]: 폰트, 아이콘, 색상 추가!

---
+++ GoWalk/GoWalk/FontManager.swift
@@ -0,0 +1,36 @@
+//
+//  FontManager.swift
+//  GoWalk
+//
+//  Created by jae hoon lee on 1/14/25.
+//
+
+import UIKit
+
+struct FontManager {
+    
+    static let sf50bold = UIFont.systemFont(ofSize: 50, weight: .heavy)
+
+    static let sf30 = UIFont.systemFont(ofSize: 30, weight: .regular)
+    static let sf30bold = UIFont.systemFont(ofSize: 30, weight: .heavy)
+
+    static let sf24 = UIFont.systemFont(ofSize: 24, weight: .regular)
+    static let sf24bold = UIFont.systemFont(ofSize: 24, weight: .bold)
+    
+    static let sf22 = UIFont.systemFont(ofSize: 22, weight: .regular)
+    static let sf22medium = UIFont.systemFont(ofSize: 22, weight: .medium)
+    static let sf22semibold = UIFont.systemFont(ofSize: 22, weight: .semibold)
+    
+    static let sf20 = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let sf20medium = UIFont.systemFont(ofSize: 20, weight: .medium)
+    static let sf20semibold = UIFont.systemFont(ofSize: 20, weight: .semibold)
+    static let sf20bold = UIFont.systemFont(ofSize: 20, weight: .bold)
+    
+    static let sf18 = UIFont.systemFont(ofSize: 18, weight: .regular)
+    static let sf18medium = UIFont.systemFont(ofSize: 18, weight: .medium)
+    
+    static let sf16 = UIFont.systemFont(ofSize: 16, weight: .regular)
+    static let sf16medium = UIFont.systemFont(ofSize: 16, weight: .medium)
+}
+
+/// 사용방법 : ex) FontManager.sf22
